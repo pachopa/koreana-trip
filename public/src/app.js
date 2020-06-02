36c638e86041663f051c063f96af2d141909dcf4
Chris Lee <chrislee910320@gmail.com>
2020-06-01 23:11:38 -0600

    final submission

--- public/src/app.js
+++ public/src/app.js
@@ -1,55 +1,25 @@
-// function confirmMsg(buttonId) {
-//   console.log('buttonId:', buttonId)
-//   document.getElementById(buttonId).addEventListener("click", function(e) {
-//     e.preventDefault();
-//     console.log("test")
-//     let confirmnation = confirm("Are you sure you want to proceed?");
-//     if(!confirmnation) {
-//       // returnToPreviousPage();
-//       return false;
-//     }
-//     return true;
-//   })
-//   // let e = e;
-//   // console.log('className:', className);
-  
-//   // if(!confirmnation) {
-//   //   console.log('e:');
-//   //   // e.preventDefault();
-//   //   // returnToPreviousPage();
-//   // } 
+$(function () {
 
-//   // return confirmnation;
-// }
-
-function confirmMsg() {
-  let confirmation = confirm("Are you sure you want to proceed?");
-  if(!confirmation ) {
-    return false;
-  } 
-  return true;
-}
-
-$(function(){
-  $(".md-form input").blur(function() {
+  //Jquery
+  $(".md-form input").blur(function () {
     let inputId = $(this).attr('id');
     let inputValue = $(this).val();
     let validation = new RegExp(/^[A-Z]\d[A-Z] ?\d[A-Z]\d$/);
     let $this = $(this);
 
-    if(inputId == 'postalCode' && !validation.test(inputValue)) {
-      
+    if (inputId == 'postalCode' && !validation.test(inputValue)) {
+
       $($this).after("<div class='notValid'>This is not valid postal code</div>");
 
       $('.notValid').fadeOut(2000);
     }
   })
 
-  $(".md-form input").focusin(function() {
+  $(".md-form input").focusin(function () {
     $(this).parent().find(".feedback").css("display", "inline-block")
   });
 
-  $(".md-form input").focusout(function() {
+  $(".md-form input").focusout(function () {
     $(this).parent().find(".feedback").css("display", "none")
   })
 
---
+++ public/src/mainFuns.js
@@ -0,0 +1,55 @@
+const localPics = ["./img/Busan.jpg", "./img/Jeju.jpg", "./img/seoul4.jpg"];
+const localPicsDes = ["Busan is the second biggest city in Korea", "Seoul is the capital city", "Jeju is the beautiful island in Korea"];
+const picsURL = ['https://english.busan.go.kr/index', 'http://english.seoul.go.kr/', 'https://www.visitjeju.net/en'];
+
+//
+//
+//
+
+let imgObjs = new Array();
+
+imgObjs[0] = new Image();
+imgObjs[0].src = localPics[0];
+imgObjs[1] = new Image();
+imgObjs[1].src = localPics[1];
+imgObjs[2] = new Image();
+imgObjs[2].src = localPics[2];
+
+function swapImage(index) {
+  let img = document.getElementById("displayImage");
+  img.src = imgObjs[index].src;
+  img.alt = index;
+  // img.addEventListener('click', function() {
+  //   console.log("chris")
+  // })
+}
+
+function listPics() {
+  for (let i = 0; i < localPicsDes.length; i++) {
+    // document.getElementsByClassName("desLists").innerHTML +=
+    //   `<li>${localPicsDes[i]}</li>`;
+    document.write(`<li onmouseover="swapImage(${i})">${localPicsDes[i]} </li>
+                    
+    `);
+  }
+}
+
+function confirmMsg() {
+  //confirm message function
+  let confirmation = confirm("Are you sure you want to proceed?");
+  if (!confirmation) {
+    return false;
+  }
+  return true;
+}
+
+function openWindow() {
+  let attr = document.getElementById("displayImage").alt
+  let picWindow = window.open(`${picsURL[attr]}`, "", "width=500, height=500");
+  setTimeout(() => {
+    picWindow.close()
+  }, 5000)
+}
+
+
+
